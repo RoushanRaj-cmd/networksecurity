044697d3f35040bdf79652a6539c457e3917c34f
RoushanRaj-cmd <roushanraj6641@gmail.com>
2025-06-16 10:21:13 +0530

    Data Transformation completed

--- networksecurity/entity/config_entity.py
+++ networksecurity/entity/config_entity.py
@@ -31,6 +31,7 @@ class DataIngestionConfig:
         self.collection_name=training_pipeline.DATA_INGESTION_COLLECTION_NAME
         self.database_name=training_pipeline.DATA_INGESTION_DATABASE_NAME
 
+
 class DataValidationConfig:
     def __init__(self, training_pipeline_config:TrainingPipelineConfig):
         self.data_validation_dir: str = os.path.join(training_pipeline_config.artifact_dir, training_pipeline.DATA_VALIDATION_DIR_NAME)
@@ -45,4 +46,14 @@ class DataValidationConfig:
             training_pipeline.DATA_VALIDATION_DRIFT_REPORT_DIR,
             training_pipeline.DATA_VALIDATION_DRIFT_REPORT_FILE_NAME
         )
+
+class DataTransformationConfig:
+    def __init__(self, training_pipeline_config:TrainingPipelineConfig):
+        self.data_transformation_dir :str = os.path.join(training_pipeline_config.artifact_dir,training_pipeline.DATA_TRANSFORMATION_DIR_NAME)
+        self.transformed_train_file_path: str = os.path.join(self.data_transformation_dir, training_pipeline.DATA_TRANSFORMATION_TRAIN_FILE,
+                                                             training_pipeline.TRAIN_FILE_NAME.replace("csv","npy"),)
+        self.transformed_test_file_path: str = os.path.join(self.data_transformation_dir, training_pipeline.DATA_TRANSFORMATION_TEST_FILE,
+                                                            training_pipeline.TEST_FILE_NAME.replace("csv","npy"),)
+        self.transformed_object_file_path: str = os.path.join(self.data_transformation_dir, training_pipeline.DATA_TRANSFORMATION_OBJECT_FILE,
+                                                              training_pipeline.PREPROCESSING_OBJECT_FILE_NAME,)
 
